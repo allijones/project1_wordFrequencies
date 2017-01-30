bd16b6ac09dcc0465907f624873e1d9ca783c54d
Grant Gasser <grant_gasser@baylor.edu>
2017-01-30 09:06:25 -0600

    Update stuString.cpp

--- project_one/project_one/stuString.cpp
+++ project_one/project_one/stuString.cpp
@@ -14,55 +14,55 @@
 #include "stuString.h"
 #include <iostream>
 
-int StuString::compare(const StuString &str){
-	int val = 0;
+int StuString::compare(const StuString &str) {
+   int val = 0;
 
-	
-	
-	return val;
+   
+
+   return val;
 }
 
-int StuString::length(){
-	int count = 0;
+int StuString::length() {
+   int count = 0;
 
-	while (data[count] != '\0') {
-		count++;
-	}
+   while (data[count] != '\0') {
+      count++;
+   }
 
-	return count;
+   return count;
 }
 
 //clears StuString for the next string/word
-void StuString::clear(){
-	data[0] = '\0';
+void StuString::clear() {
+   data[0] = '\0';
 }
 
 //need to dynamically resize in here
-void StuString::addChar(char c){
-	//to clean up code in rest of function
-	int length = this->length();
-	int oldCap = this->capacity;
-	cout << "Length: " << length << endl;
-	cout << "Cap: " << oldCap << endl;
+void StuString::addChar(char c) {
+   //to clean up code in rest of function
+   int length = this->length();
+   int oldCap = this->capacity;
+   //cout << "Length: " << length << endl;
+   cout << "Cap: " << oldCap << endl;
 
-	//double capacity
-	if (length + 1 == oldCap) {
-		cout << "In double cap function" << endl;
-		//temporary pointer so as not to lose address of init array when copying
-		char *temp = data;
+   //double capacity
+   if (length + 1 == oldCap) {
+      //temporary pointer so as not to lose address of init array when copying
+      char *temp = data;
 
-		this->capacity *= 2;
-		data = new char[this->capacity];
+      //double capacity of char array
+      this->capacity *= 2;
+      data = new char[this->capacity];
 
-		//copy values of old array into new array
-		for (int i = 0; i < oldCap; i++) {
-			data[i] = temp[i];
-		}
+      //copy values of old array into new array
+      for (int i = 0; i < oldCap; i++) {
+         data[i] = temp[i];
+      }
 
-		delete[] temp;
-	}
+      delete[] temp;
+   }
 
-	//add the character and null after it
-	data[length] = c;
-	data[length + 1] = '\0';
+   //add the character and null after it
+   data[length] = c;
+   data[length + 1] = '\0';
 }
