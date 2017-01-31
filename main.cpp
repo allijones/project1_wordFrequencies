b862a91a0ae66d57106354c9d6ab3352c5b1ae35
Grant Gasser <grant_gasser@baylor.edu>
2017-01-30 21:56:29 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@
 #include <iostream>
 #include <ctype.h>
 #include <fstream>
+#include <iomanip>
 
 using namespace std;
 
@@ -23,10 +24,10 @@ int main() {
 	ifstream inFile;
 	string fileName;
 	StuString stuStr, str2;
-	Word word;
+	Word word, tempWord;
 	char ch;
 	char lowCh;
-	int amtPunct, amtSpace;
+	int amtPunct, amtSpace, amtDigit;
 	int wordCap = 2, wordCount = 0;
 
 	//Test for file open
@@ -47,14 +48,15 @@ int main() {
 	Word *wordArr = new Word[wordCap]; 
 
 	while (inFile.get(ch)) {
-		if (!ispunct(ch) && !isspace(ch)) {
+		if (!ispunct(ch) && !isspace(ch) && !isdigit(ch)) {
 			amtPunct = 0;
 			amtSpace = 0;
+			amtDigit = 0;
 			//make lowercase
 			lowCh = tolower(ch);
 			//add to StuString
 			stuStr.addChar(lowCh);
-			cout << "stuStr: " << stuStr.toString() << endl;
+			//cout << "stuStr: " << stuStr.toString() << endl;
 		}
 		else if (ispunct(ch)) {
 			amtPunct++;
@@ -62,16 +64,23 @@ int main() {
 		else if (isspace(ch)) {
 			amtSpace++;
 		}
+		else if (isdigit(ch)) {
+			amtDigit++;
+		}
 
-		//only set the word if there has been only 1 space or 1 punctuation
-		if ((amtPunct == 1 && amtSpace == 0) || (amtPunct == 0 && amtSpace == 1)) {
-			cout << "Length of StuString about to be added to words: " << stuStr.length() << endl;
-			wordArr[wordCount].setWord(stuStr);
-			wordCount++;
-			stuStr.clear();
-			str2 = wordArr[wordCount-1].getData();
-			cout << endl;
-			cout << "Word: " << str2.toString() << endl << endl;
+		//only set the word if there has been only 1 space or 1 punctuation or 1 digit
+		if ((amtPunct == 1 && amtSpace == 0 && amtDigit == 0) || 
+			(amtPunct == 0 && amtSpace == 1 && amtDigit == 0) ||
+			(amtPunct == 0 && amtSpace == 0 && amtDigit == 1)) {
+				wordArr[wordCount].setWord(stuStr);
+				wordCount++;
+				stuStr.clear();
+				str2 = wordArr[wordCount-1].getData();
+				//cout << endl;
+				/*cout << "Word: " << str2.toString() << endl << endl;
+
+				cout << "wordArr[wordCount-1].getData(): " << wordArr[wordCount-1].getData().toString() << endl;
+				cout << "Should get zeros:  " << str2.compare(wordArr[wordCount-1].getData()) << endl;*/
 		}
 
 		if (wordCount == wordCap) {
@@ -92,12 +101,41 @@ int main() {
 		}
 	}
 
+	//FIND FREQUENCIES HERE
+	//passes
+	for (int i = 0; i < wordCount; i++) {
+		for (int j = i + 1; j < wordCount; j++) {
+			if (wordArr[i].getData().compare(wordArr[j].getData()) == 0) {
+				wordArr[i].incrFrequency(wordArr[i].getData());
+				wordArr[j].incrFrequency(wordArr[j].getData());
+			}
+		}
+	}
+
+	//Sort the words here
+	for (int pass = 1; pass < wordCount; pass++) {
+		for (int j = 0; j < wordCount - pass; j++) {
+			if (wordArr[pass].getData().compare(wordArr[j].getData())) {
+				//swap words
+				//store freq. and str of object in tempWord
+				tempWord.setFrequency(wordArr[j].getFrequency());
+				tempWord.setWord(wordArr[j].getData());
+
+				//replace pass object data with j object data
+				wordArr[j].setFrequency(wordArr[j+1].getFrequency());
+				wordArr[j].setWord(wordArr[j+1].getData());
+
+				//replace j object data with tempWord object data (pass obj data)
+				wordArr[j+1].setFrequency(tempWord.getFrequency());
+				wordArr[j+1].setWord(tempWord.getData());
+			}
+		}
+	}
+
 	//PRINT WORDS TEST -- IT WORKS
 	for (int i = 0; i < wordCount; i++) {
-		cout << wordArr[i].getWord().toString() << endl;
+		cout << setw(15) << left << wordArr[i].getWord().toString() << right << wordArr[i].getFrequency() << endl;
 	}
-	cout << endl;
-	cout << "DONE." << endl;
 	
 	//Housekeeping
 	inFile.close();
