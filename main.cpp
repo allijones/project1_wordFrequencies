054a8b87f39eba3b879ea82b576ba3c8e89a6404
Grant Gasser <grant_gasser@baylor.edu>
2017-01-31 15:30:49 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -21,127 +21,133 @@
 using namespace std;
 
 int main() {
-	ifstream inFile;
-	string fileName;
-	StuString stuStr, str2;
-	Word word, tempWord;
-	char ch;
-	char lowCh;
-	int amtPunct, amtSpace, amtDigit;
-	int wordCap = 2, wordCount = 0;
+   ifstream inFile;
+   string fileName;
+   StuString stuStr, str2;
+   Word word, tempWord;
+   char ch;
+   char lowCh;
+   int amtPunct, amtSpace, amtDigit;
+   int wordCap = 2, wordCount = 0;
 
-	//Test for file open
-	do {
-		cout << "Enter Name of File: ";
-		cin >> fileName;
+   //Test for file open
+   do {
+      cout << "Enter Name of File: ";
+      cin >> fileName;
 
-		inFile.clear();
-		inFile.open(fileName.c_str());
+      inFile.clear();
+      inFile.open(fileName.c_str());
 
-		if (!inFile) {
-			cout << "Error: File Not Open." << endl;
-		}
+      if (!inFile) {
+         cout << "Error: File Not Open." << endl;
+      }
 
-	} while (!inFile);
+   } while (!inFile);
 
-	//dynamically allocate array of Words 
-	Word *wordArr = new Word[wordCap]; 
+   //dynamically allocate array of Words 
+   Word *wordArr = new Word[wordCap];
 
-	while (inFile.get(ch)) {
-		if (!ispunct(ch) && !isspace(ch) && !isdigit(ch)) {
-			amtPunct = 0;
-			amtSpace = 0;
-			amtDigit = 0;
-			//make lowercase
-			lowCh = tolower(ch);
-			//add to StuString
-			stuStr.addChar(lowCh);
-			//cout << "stuStr: " << stuStr.toString() << endl;
-		}
-		else if (ispunct(ch)) {
-			amtPunct++;
-		}
-		else if (isspace(ch)) {
-			amtSpace++;
-		}
-		else if (isdigit(ch)) {
-			amtDigit++;
-		}
+   while (inFile.get(ch)) {
+      if (!ispunct(ch) && !isspace(ch) && !isdigit(ch)) {
+         amtPunct = 0;
+         amtSpace = 0;
+         amtDigit = 0;
+         //make lowercase
+         lowCh = tolower(ch);
+         //add to StuString
+         stuStr.addChar(lowCh);
+         //cout << "stuStr: " << stuStr.toString() << endl;
+      }
+      else if (ispunct(ch)) {
+         amtPunct++;
+      }
+      else if (isspace(ch)) {
+         amtSpace++;
+      }
+      else if (isdigit(ch)) {
+         amtDigit++;
+      }
 
-		//only set the word if there has been only 1 space or 1 punctuation or 1 digit
-		if ((amtPunct == 1 && amtSpace == 0 && amtDigit == 0) || 
-			(amtPunct == 0 && amtSpace == 1 && amtDigit == 0) ||
-			(amtPunct == 0 && amtSpace == 0 && amtDigit == 1)) {
-				wordArr[wordCount].setWord(stuStr);
-				wordCount++;
-				stuStr.clear();
-				str2 = wordArr[wordCount-1].getData();
-				//cout << endl;
-				/*cout << "Word: " << str2.toString() << endl << endl;
+      //only set the word if there has been only 1 space or 1 punctuation or 1 digit
+      if ((amtPunct == 1 && amtSpace == 0 && amtDigit == 0) ||
+         (amtPunct == 0 && amtSpace == 1 && amtDigit == 0) ||
+         (amtPunct == 0 && amtSpace == 0 && amtDigit == 1)) {
+         wordArr[wordCount].setWord(stuStr);
+         wordCount++;
+         stuStr.clear();
+         str2 = wordArr[wordCount - 1].getData();
+         //cout << endl;
+         /*cout << "Word: " << str2.toString() << endl << endl;
+         cout << "wordArr[wordCount-1].getData(): " << wordArr[wordCount-1].getData().toString() << endl;
+         cout << "Should get zeros:  " << str2.compare(wordArr[wordCount-1].getData()) << endl;*/
+      }
 
-				cout << "wordArr[wordCount-1].getData(): " << wordArr[wordCount-1].getData().toString() << endl;
-				cout << "Should get zeros:  " << str2.compare(wordArr[wordCount-1].getData()) << endl;*/
-		}
+      if (wordCount == wordCap) {
+         //temp pointer to Word array so as to not lot as address when need to copy
+         Word *temp = wordArr;
 
-		if (wordCount == wordCap) {
-			//temp pointer to Word array so as to not lot as address when need to copy
-			Word *temp = wordArr;
+         //double word capacity
+         wordCap *= 2;
 
-			//double word capacity
-			wordCap *= 2;
+         wordArr = new Word[wordCap];
 
-			wordArr = new Word[wordCap];
+         //copy values
+         for (int i = 0; i < wordCount; i++) {
+            wordArr[i] = temp[i];
+         }
 
-			//copy values
-			for (int i = 0; i < wordCount; i++) {
-				wordArr[i] = temp[i];
-			}
+         delete[] temp;
+      }
+   }
 
-			delete[] temp;
-		}
-	}
+   //FIND FREQUENCIES HERE
+   //passes
+   for (int i = 0; i < wordCount; i++) {
+      for (int j = i + 1; j < wordCount; j++) {
+         if (wordArr[i].getData().compare(wordArr[j].getData()) == 0) {
+            wordArr[i].incrFrequency(wordArr[i].getData());
+            wordArr[j].incrFrequency(wordArr[j].getData());
+         }
+      }
+   }
 
-	//FIND FREQUENCIES HERE
-	//passes
-	for (int i = 0; i < wordCount; i++) {
-		for (int j = i + 1; j < wordCount; j++) {
-			if (wordArr[i].getData().compare(wordArr[j].getData()) == 0) {
-				wordArr[i].incrFrequency(wordArr[i].getData());
-				wordArr[j].incrFrequency(wordArr[j].getData());
-			}
-		}
-	}
+   //Sort the words here
+   for (int pass = 1; pass < wordCount; pass++) {
+      for (int j = 0; j < wordCount - pass; j++) {
+         if (wordArr[j].getData().compare(wordArr[j+1].getData()) > 0) {
+            //swap words
+            //store freq. and str of object in tempWord
+            tempWord.setFrequency(wordArr[j].getFrequency());
+            tempWord.setWord(wordArr[j].getData());
 
-	//Sort the words here
-	for (int pass = 1; pass < wordCount; pass++) {
-		for (int j = 0; j < wordCount - pass; j++) {
-			if (wordArr[pass].getData().compare(wordArr[j].getData())) {
-				//swap words
-				//store freq. and str of object in tempWord
-				tempWord.setFrequency(wordArr[j].getFrequency());
-				tempWord.setWord(wordArr[j].getData());
+            //replace pass object data with j object data
+            wordArr[j].setFrequency(wordArr[j + 1].getFrequency());
+            wordArr[j].setWord(wordArr[j + 1].getData());
 
-				//replace pass object data with j object data
-				wordArr[j].setFrequency(wordArr[j+1].getFrequency());
-				wordArr[j].setWord(wordArr[j+1].getData());
+            //replace j object data with tempWord object data (pass obj data)
+            wordArr[j + 1].setFrequency(tempWord.getFrequency());
+            wordArr[j + 1].setWord(tempWord.getData());
+         }
+      }
+   }
 
-				//replace j object data with tempWord object data (pass obj data)
-				wordArr[j+1].setFrequency(tempWord.getFrequency());
-				wordArr[j+1].setWord(tempWord.getData());
-			}
-		}
-	}
+   //PRINT WORDS TEST -- IT WORKS
+   for (int i = 0; i < wordCount; i++) {
+      //making sure to not print same word twice
+      if (i == 0) {
+         //print first word, can't be a duplicate
+         cout << setw(15) << left << wordArr[i].getData().toString() << right << wordArr[i].getFrequency() << endl;
+      } else if(wordArr[i].getData().compare(wordArr[i - 1].getData()) != 0) {
+         //only printing if word has not been printed
+         cout << setw(15) << left << wordArr[i].getData().toString() << right << wordArr[i].getFrequency() << endl;
+      }
+   }
 
-	//PRINT WORDS TEST -- IT WORKS
-	for (int i = 0; i < wordCount; i++) {
-		cout << setw(15) << left << wordArr[i].getWord().toString() << right << wordArr[i].getFrequency() << endl;
-	}
-	
-	//Housekeeping
-	inFile.close();
+   //Housekeeping
+   inFile.close();
 
-	//HEAP ERROR when I call this. Why??
-	//delete[] wordArr; 
+   //HEAP ERROR when I call this. Why??
+   //delete[] wordArr; 
 
-	return 0;
+   return 0;
 }
